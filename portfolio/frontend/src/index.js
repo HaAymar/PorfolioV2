8cb08bac43fae83f5dfe0140298cb8f3362c39fe
HaAymar <he201905@students.ephec.be>
2025-12-08 00:35:20 +0100

    mise à jour du deploiement

--- portfolio/frontend/src/index.js
+++ portfolio/frontend/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
 import { Toaster } from "@/components/ui/toaster";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-    <Toaster />
-  </React.StrictMode>,
+    <BrowserRouter basename="/PorfolioV2">
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </React.StrictMode>
 );
